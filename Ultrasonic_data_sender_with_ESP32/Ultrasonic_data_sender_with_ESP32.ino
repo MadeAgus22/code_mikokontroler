ced6b214b0451506df47c5fc23b1b142702260b4
Made Agus Toni Saputra <131594593+MadeAgus22@users.noreply.github.com>
2024-08-29 17:29:08 +0800

    Add files via upload

---
+++ Ultrasonic_data_sender_with_ESP32/Ultrasonic_data_sender_with_ESP32.ino
@@ -0,0 +1,54 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define TRIGGER_PIN  12
+#define ECHO_PIN     14
+
+const char* ssid = "IoT";
+const char* password = "qwerty1234";
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  long duration, distance;
+  
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = duration * 0.034 / 2;  // Convert to cm
+
+  if(WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    http.begin("http://192.168.145.181:8080/customer");  // Ganti dengan URL endpoint API Anda
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String postData = "names=" + String(distance); // names = diganti sesuai post data
+    int httpResponseCode = http.POST(postData);
+
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(10000);  // Kirim data setiap 10 detik
+}
